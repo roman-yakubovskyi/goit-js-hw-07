58f02fd54fb94df29fb7f4326dda430a26181890
roman-yakubovskyi <yakubovskyi133@gmail.com>
2024-10-19 01:14:59 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', checkValid);
+function checkValid(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return alert('All form fields must be filled in');
+  }
+  const userInfo = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+  console.log(userInfo);
+  event.currentTarget.reset();
+}
